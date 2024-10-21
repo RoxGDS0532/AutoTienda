7acd522c07cfba01e186b128a46b61de858a5c89
elizabethgutierrez27 <146129308+elizabethgutierrez27@users.noreply.github.com>
2024-10-21 16:35:01 -0600

    Componente Eli

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {UserComponent } from './components/user/user.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
     {path: '', component: UserComponent},
+    {path:'login',component:LoginComponent}
 ];
---
+++ frontend/src/app/components/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const loginData = this.loginForm.value;
+      // Aquí puedes llamar a tu servicio de autenticación
+      console.log('Login data:', loginData);
+    }
+  }
+}

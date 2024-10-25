7f9b4cdafea3a8384e3ea7c994d9a5aa68604cc4
RoxGDS0532 <rox.renteria1234@gmail.com>
2024-10-25 11:28:33 -0600

    pagina inicial

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {UserComponent } from './components/user/user.component';
 import { LoginComponent } from './components/login/login.component';
+import { CarritoComponent } from './components/carrito/carrito.component';
 
 export const routes: Routes = [
     {path: '', component: UserComponent},
-    {path:'login',component:LoginComponent}
+    {path:'login',component:LoginComponent},
+    {path:'carrito', component: CarritoComponent}
 ];

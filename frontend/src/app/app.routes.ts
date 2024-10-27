548a1a3b3afbb9a0604a3d8a3e94d743532b09ac
elizabethgutierrez27 <146129308+elizabethgutierrez27@users.noreply.github.com>
2024-10-26 22:19:01 -0600

    inventario

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import {UserComponent } from './components/user/user.component';
 import { LoginComponent } from './components/login/login.component';
 import { CarritoComponent } from './components/carrito/carrito.component';
+import { InventarioComponent } from './components/inventario/inventario.component';
 
 export const routes: Routes = [
     {path: '', component: UserComponent},
     {path:'login',component:LoginComponent},
-    {path:'carrito', component: CarritoComponent}
+    {path:'carrito', component: CarritoComponent},
+    {path:'inventario',component:InventarioComponent}
 ];

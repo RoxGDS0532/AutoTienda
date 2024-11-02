8bb847170bea21fde508c9de0fcc7f69de7ecbc6
RoxGDS0532 <rox.renteria1234@gmail.com>
2024-11-02 13:38:54 -0600

    funcionalidad carrito

--- frontend/src/app/components/carrito/carrito.component.ts
+++ frontend/src/app/components/carrito/carrito.component.ts
@@ -1,16 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductoService, Producto } from '../../services/producto.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // Importar FormsModule
+import { BrowserMultiFormatReader } from '@zxing/browser';
 @Component({
   selector: 'app-carrito',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './carrito.component.html',
   styleUrl: './carrito.component.css'
 })
 export class CarritoComponent implements OnInit {
-  producto: Producto | undefined;
+  productos:  Producto[] = [];
+  codeReader = new BrowserMultiFormatReader();
+  codigoDescuento: string = '';
+  descuentoAplicado: boolean = false;
+  
+  @ViewChild('video') videoElement: ElementRef | undefined;
 
   constructor(
     private route: ActivatedRoute,
@@ -20,11 +27,75 @@ export class CarritoComponent implements OnInit {
   ngOnInit(): void {
     const codigoBarras = this.route.snapshot.paramMap.get('codigoBarras');
     if (codigoBarras) {
-      this.productoService.obtenerProductoPorCodigoBarras(+codigoBarras)
-        .subscribe(
-          producto => this.producto = producto,
-          error => console.error('Error al obtener el producto:', error)
-        );
+      this.buscarProductoPorCodigo(+codigoBarras);
     }
+  }
+
+  iniciarEscaneo(): void {
+    if (this.videoElement) {
+      this.codeReader.decodeOnceFromVideoDevice(undefined, this.videoElement.nativeElement)
+        .then(result => {
+          const codigoEscaneado = +result.getText();
+
+          const beepSound = new Audio('assets/sound/beep.mp3');
+          beepSound.play();
+
+          console.log('Código escaneado:', codigoEscaneado);
+          this.buscarProductoPorCodigo(codigoEscaneado);
+        })
+        .catch(err => console.error('Error al escanear:', err));
+    }
+  }
+
+  buscarProductoPorCodigo(codigoBarras: number): void {
+    const productoExistente = this.productos.find(p => p.CodigoBarras === codigoBarras);
+
+    if (productoExistente) {
+      productoExistente.Cantidad += 1;
+    } else {
+      this.productoService.obtenerProductoPorCodigoBarras(codigoBarras).subscribe({
+        next: (data) => {
+          data.Cantidad = 1; 
+          this.productos.push(data); 
+        },
+        error: (err) => console.error('Producto no encontrado', err)
+      });
+    }
+  }
+
+  incrementarCantidad(producto: Producto): void {
+    producto.Cantidad += 1;
+  }
+  
+  decrementarCantidad(producto: Producto): void {
+    if (producto.Cantidad > 1) {
+      producto.Cantidad -= 1;
+    }else{
+      this.eliminarProducto(producto);
+    }
+  }
+  
+  eliminarProducto(producto: Producto): void {
+    this.productos = this.productos.filter(p => p !== producto);
+  }
+
+    obtenerSubtotal(): number {
+      return this.productos.reduce((acc, producto) => acc + producto.Precio * producto.Cantidad, 0);
+    }
+  
+    obtenerAhorros(): number {
+      if (this.descuentoAplicado) {
+        return this.productos.reduce((acc, producto) => acc + producto.Precio * producto.Cantidad * 0.1, 0); // Descuento del 10%
+      }
+      return 0;
+    }
+
+    aplicarCodigoDescuento(): void {
+      const codigoValido = 'DESCUENTO10'; // Ejemplo de un código de descuento
+      this.descuentoAplicado = this.codigoDescuento === codigoValido;
+    }
+
+  obtenerTotal(): number {
+    return this.obtenerSubtotal() - this.obtenerAhorros();
   }
 }

6b26951695943acfdb576e62287296f6d5a66ced
RoxGDS0532 <rox.renteria1234@gmail.com>
2024-11-24 10:55:52 -0600

    .

--- frontend/src/app/state-producto/agotado.estado.ts
+++ frontend/src/app/state-producto/agotado.estado.ts
@@ -1,6 +1,6 @@
 import { EstadoProducto } from './producto.interface';
 import { Producto } from '../services/producto.service';
-
+/*Falta acciones aún, solo es estructura :) */
 export class Agotado implements EstadoProducto {
   manejarEstado(producto: Producto): void {
     console.log(`El producto ${producto.Nombre} está agotado.`);
